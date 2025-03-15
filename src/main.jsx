d9a4f9be89ef7dae49925be0e7481f56b33039d9
cycodedconcept <cycodedconcept@gmail.com>
2025-03-14 17:06:59 -1200

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
- import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux';
-// import './index.css';
 import App from './App.jsx';
 import store from './store/store';
 import 'bootstrap/dist/css/bootstrap.min.css';
